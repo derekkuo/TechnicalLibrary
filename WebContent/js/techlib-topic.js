ce918263ccd880bdb5601765b0738c51a2d042f1
Derek Kuo <derekkuo@263.net>
2011-03-27 03:40:38 +0800

    去除java-io.html和techlib-topic.js中无用的注释

--- WebContent/js/techlib-topic.js
+++ WebContent/js/techlib-topic.js
@@ -41,41 +41,6 @@
 					$(this).prepend("<div style='text-align: right\;'><a href='#top'>TOP</a></div>");
 			$(this).prepend("<a name='"+i+"'></a>");
 		});
-		
-		//生成章以及节的二层目录
-		/*
-		var $chapters = $(".part");
-		$chapters.each(function(i){
-			$("#menu").empty();
-			$("#menu").append( $(this).html() );
-			var $chapter = $("#menu > .chapter");
-			//alert($chapter.text());
-			$("#menu").empty();
-			$("#menu").append( $(this).html() );
-			var $sections = $("#menu > .section");
-			$sections.each(function(j){
-				//alert($(this).text());
-			});
-		});
-		$("#menu").remove();
-		*/
-		
-		//生成目录
-		/*
-		var $h2s = $("h2");
-		var $menubarBlock = $("#techlib-block-menubar");
-		$menubarBlock.append("<div id='techlib-menubar'><div style='font-size:16px\; font-weight:bold\; margin-top:15px\; margin-bottom:15px\;'>目录</div><UL class='techlib-menubar-ul'></UL></div>");
-		var $menubarUl = $(".techlib-menubar-ul");
-		$h2s.each(function(i){
-			i++;
-			//添加目录
-			$menubarUl.append("<LI><A href='#"+i+"'>"+$(this).html()+"</A></LI>");
-			//正文中添加锚点
-			if(i!=1 && i!=$h2s.length)//1第一节不打印 i!=$h2s.length最后一节不打印
-				$(this).prepend("<div style='text-align: right\;'><a href='#top'>TOP</a></div>");
-			$(this).prepend("<a name='"+i+"'></a>");
-		});
-		*/
 	});
 /*
 </script>
