4e6bdf04496175f23329dbee1ab588dd97cfdcd1
Derek Kuo <derekkuo@263.net>
2011-04-06 19:24:45 +0800

    完成了读取WebContent目录中文件的测试

--- src/util/SpecifiedTask.java
+++ src/util/SpecifiedTask.java
@@ -1,5 +1,10 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.util.TimerTask;
 
 import javax.servlet.ServletContext;
@@ -15,8 +20,27 @@ public class SpecifiedTask extends TimerTask {
         if(!isRunning){
                 isRunning=true;
                 System.out.println("生成目录...........");
-                
+                readHtml();
         }
     }
-
+    
+    private void readHtml(){
+    	InputStream is = null;
+		try {
+			is = context.getResourceAsStream("/topic/git-quick-starts.html");
+		
+			String str;
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			while( (str = br.readLine()) != null){
+				System.out.println(str);
+				if(str.indexOf("title")>-1)
+					break;
+			}
+		} catch (Exception e1) {
+//			e1.printStackTrace();
+		}
+    	
+    	
+    	
+    }
 }
